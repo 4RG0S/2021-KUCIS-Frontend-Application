b9b325c5d45e648eb77937a1c61de81d9385a945
23581321 <33508696+23581321@users.noreply.github.com>
2021-07-10 20:20:56 +0900

    Update SignIn.js

--- frontend-react/src/pages/SignIn.js
+++ frontend-react/src/pages/SignIn.js
@@ -1,9 +1,105 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import SignUp from './SignUp';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  signup: {
+    margin: theme.spacing(0, 0, 2),  
+  },
+}));
 
 export default function SignIn() {
+  const classes = useStyles();
+
   return (
-      <div>
-          <h1>This is SignIn Component</h1>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Link to="./SignUp">
+          <Button
+            type="button"
+            href="/sign-up"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.signup}
+          >
+            Sign Up
+          </Button>
+          </Link>
+          <Grid container>
+            <Grid item xs>
+              Did you Forget the password?
+              <Link href="#" variant="body2">
+                Find Password
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
       </div>
+    </Container>
   );
 }
